ca12c67f752d70276bd7141b416ab417bda1edee
nayanprasad <nayanprasad096@gmail.com>
2025-02-21 05:13:34 +0530

    testing

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,23 +1,23 @@
 import sharedConfig from "@repo/tailwind-config/tailwind.config";
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 const config: Pick<Config, "presets"> = {
-  presets: [
-    {
-      ...sharedConfig,
-      content: [
-        "./app/**/*.{js,ts,jsx,tsx}",
-        "./ui/**/*.{js,ts,jsx,tsx}",
-        "../../packages/ui/src/**/*{.js,.ts,.jsx,.tsx}",
-        "../../packages/blocks/src/**/*{.js,.ts,.jsx,.tsx}",
-      ],
-      theme: {
-        extend: {
-          ...sharedConfig?.theme?.extend,
+    presets: [
+        {
+            ...sharedConfig,
+            content: [
+                "./app/**/*.{js,ts,jsx,tsx}",
+                "./ui/**/*.{js,ts,jsx,tsx}",
+                "../../packages/ui/src/**/*{.js,.ts,.jsx,.tsx}",
+                "../../packages/blocks/src/**/*{.js,.ts,.jsx,.tsx}",
+            ],
+            theme: {
+                extend: {
+                    ...sharedConfig?.theme?.extend,
+                },
+            },
         },
-      },
-    },
-  ],
+    ],
 };
 
 export default config;
