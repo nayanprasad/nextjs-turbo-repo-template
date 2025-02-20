4b6ae41e8bc4966fd3a4b4bad2fef4345f8793f6
nayanprasad <nayanprasad096@gmail.com>
2025-02-21 05:07:41 +0530

    fixes

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -8,7 +8,6 @@ const config: Pick<Config, "presets"> = {
             content: [
                 "./app/**/*.{js,ts,jsx,tsx}",
                 "./ui/**/*.{js,ts,jsx,tsx}",
-                // h/t to https://www.willliu.com/blog/Why-your-Tailwind-styles-aren-t-working-in-your-Turborepo
                 "../../packages/ui/src/**/*{.js,.ts,.jsx,.tsx}",
                 "../../packages/blocks/src/**/*{.js,.ts,.jsx,.tsx}",
             ],
