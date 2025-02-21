23aaa6b0e8124a5527f40b1d1a8046177e98746b
nayanprasad <nayanprasad096@gmail.com>
2025-02-21 16:37:22 +0530

    updated config + added shadcn

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,14 @@
-"use client";
-import { Button } from "@repo/ui/button";
+import { Button } from "@workspace/ui/components/button";
+import { Calendar } from "@workspace/ui/components/calendar";
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className={"bg-red-800 p-32"}>
-      <Button appName={"a"}>Click me</Button>
+    <div className="flex min-h-svh items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">Hello World</h1>
+        <Button size="sm">Button</Button>
+        <Calendar />
+      </div>
     </div>
   );
 }
